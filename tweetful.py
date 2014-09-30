8ee02081f10576832ef6eaebecbef2d2cc7d1ea5
Peter Skipper <peter.skipper@gmail.com>
2014-09-30 13:27:59 -0700

    Added popularity function

--- tweetful.py
+++ tweetful.py
@@ -3,7 +3,6 @@ import json
 import requests
 from collections import defaultdict
 import operator
-
 from urls import *
 
 def where_from(timeline):
@@ -24,20 +23,31 @@ def where_from(timeline):
 
     return total, locs
 
+def popular(friend_list, size):
+    followers = []
+    for friend in friend_list["users"]:
+        followers.append((friend["name"],friend["followers_count"]))
+    sorted_followers = sorted(followers, key=operator.itemgetter(1),
+        reverse=True)
+    return sorted_followers[:size]
 
 def main():
     """Main function"""
     auth = authorization.authorize()
-    response = requests.get(TIMELINE_URL, auth=auth)
+    response = requests.get(FRIENDS_URL, auth=auth)
+    pop_list = popular(response.json(),10)
+    for item in pop_list:
+        print "{} : {:,} followers".format(item[0], item[1])
+    #response = requests.get(TIMELINE_URL, auth=auth)
     #print json.dumps(response.json(), indent=4)
-    total, locs = where_from(response.json())
-    sorted_locs = sorted(locs.iteritems(),
-        key=operator.itemgetter(1),
-        reverse=True)
-    print total
+    #total, locs = where_from(response.json())
+    #sorted_locs = sorted(locs.iteritems(),
+    #    key=operator.itemgetter(1),
+    #    reverse=True)
+    #print total
 
-    for loc, tweets in sorted_locs:
-        print "{}: {:.1%}".format(loc, tweets/float(total))
+    #for loc, tweets in sorted_locs:
+    #    print "{}: {:.1%}".format(loc, tweets/float(total))
 
 
 
